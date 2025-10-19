8483a82ea0cfb2fcc12aa85522b8855dd5889b5c
Shahroz Allauddin <smshahroz.allauddin@gmail.com>
2025-10-19 18:20:57 +0500

    Build error fixes

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { CssBaseline } from "@mui/material";
+
 import "./index.scss";
 import App from "./App";
 
@@ -7,6 +9,7 @@ const container = document.getElementById('root');
 const root = createRoot(container!);
 root.render(
   <React.StrictMode>
+    <CssBaseline />
     <App />
   </React.StrictMode>
 );
